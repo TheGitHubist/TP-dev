d8272bcb623d2baa7a76acae086958b0607fbb07
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-10-17 15:10:17 +0200

    organisation & tp 3 - 4

---
+++ tp-4/b2_client_I1.py
@@ -0,0 +1,14 @@
+import socket
+
+host = '10.1.1.22'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+s.sendall(b'Meooooooo !')
+data = s.recv(1024)
+
+s.close()
+
+print(f"Le serveur a répondu {repr(data)}")
