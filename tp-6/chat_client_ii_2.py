5f1b5efcb27dca995a76c5e88a2e47908583c986
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-10-30 16:42:00 +0100

    suite tp

--- tp-6/chat_client_ii_2.py
+++ tp-6/chat_client_ii_2.py
@@ -6,11 +6,8 @@ port = 8888
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
+print(f"Connecté avec succès au serveur {host} sur le port {port}")
 
-s.sendall(b'Meooooooo !')
+message = input('Entrez ce que vous souhaitez envoyer au serveur : ')
+s.sendall(message.encode('utf-8'))
 data = s.recv(1024)
-
-s.close()
-
-print(f"Le serveur a répondu {repr(data)}")
-sys.exit(0)
