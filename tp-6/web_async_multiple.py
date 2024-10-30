133e5fc132be80111ecb52db7b362617ecab0fed
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-10-30 15:38:32 +0100

    suite tp

--- tp-6/web_async_multiple.py
+++ tp-6/web_async_multiple.py
@@ -12,8 +12,12 @@ async def write_content(content, file):
     async with aiofiles.open(file, 'w') as f:
         await f.write(content)
 
-async def main(url_list):
-    tasks = [write_content(await get_content(url), '/tmp/web_async_' + url.split("/")[-1]) for url in url_list]
+async def main(url_list_file):
+    urls = []
+    with open(url_list_file, 'r') as f:
+        for line in f:
+            urls.append(line.strip())
+    tasks = [write_content(await get_content(url), '/tmp/web_' + url.split("/")[-1]) for url in urls]
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
