4e68198cb3dcfab33d3f2957043e8408d98cf4c9
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-10-31 17:35:40 +0100

    suite tp

--- tp-6/chat_server_ii_4.py
+++ tp-6/chat_server_ii_4.py
@@ -35,10 +35,12 @@ async def handle_client_msg(reader, writer):
                     CLIENTS[addrs]['w'].write(f"{bcolors.OKBLUE}{addr[0]}:{bcolors.OKGREEN}{addr[1]} {bcolors.HEADER}:> {messList[0]}{bcolors.ENDC}".encode())
                     spaces = " " * len(f'{addr[0]}:{addr[1]}:> ')
                     for line in messList[1:]:
+                        CLIENTS[addrs]['w'].write(b"\n")
                         CLIENTS[addrs]['w'].write(f"{spaces} {bcolors.HEADER}{line}{bcolors.ENDC}".encode())
                 else:
                     print("only one")
                     CLIENTS[addrs]["w"].write(f"{bcolors.OKBLUE}{addr[0]}:{bcolors.OKGREEN}{addr[1]} {bcolors.HEADER}:> {messList[0]}{bcolors.ENDC}".encode())
+                CLIENTS[addrs]['w'].write(b"\n")
                 await CLIENTS[addrs]["w"].drain()
                 print(f"message sent from {addr} to {addrs}")
             else:
