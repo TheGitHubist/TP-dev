541508c2501e93a5bf06d510bd6f39cd86e4957b
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-11-21 14:43:27 +0100

    reorg

---
+++ tp-6/chat_client_ii_4.py
@@ -0,0 +1,54 @@
+import socket
+import sys
+import aioconsole
+import asyncio
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('10.1.1.22',8888))
+
+async def asInput(r, w) :
+    while True:
+        lines = []
+        while True:
+            ZaLine = await aioconsole.ainput()
+            if not ZaLine:
+                    break
+            lines.append(ZaLine)
+        line = '\n'.join(lines)
+        if line == 'exit':
+            sys.exit(0)
+        w.write(line.encode())
+        await w.drain()
+
+
+async def asRecieve(r, w) :
+    while True:
+        data = await r.read(1024)
+        if not data:
+            break
+        print(f"{data.decode()}")
+
+async def main() :
+    pseudo = input("Enter your username : ")
+    s.sendall(()'Hello|' + pseudo).encode())
+    reader, writer = await asyncio.open_connection(host="10.1.1.22", port=8888)
+    tasks = [asInput(reader, writer), asRecieve(reader, writer)]
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    s.close()
+    print("Connexion fermee")
+
+sys.exit(0)
---
+++ tp-6/chat_server_ii_5.py
@@ -0,0 +1,66 @@
+import asyncio
+global CLIENTS
+CLIENTS = {}
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+async def handle_client_msg(reader, writer):
+    while True:
+        data = await reader.read(1024)
+        addr = writer.get_extra_info('peername')
+        pseudo = data.decode()
+        if 'Hello|' in pseudo :
+            pseudo = pseudo.split('|')[1]
+            print(f"{bcolors.OKGREEN}{pseudo}{bcolors.ENDC} connected from {addr}")
+        CLIENTS[addr] = {}
+        CLIENTS[addr]['w'] = writer
+        CLIENTS[addr]['r'] = reader
+        CLIENTS[addr]['pseudo'] = pseudo
+
+        if data == b'':
+            break
+
+        message = data.decode()
+        for addrs in CLIENTS.keys():
+            print(addrs)
+            if addrs[0] != addr[0]:
+                messList = message.split("\n")
+                print(messList)
+                if len(messList) > 1:
+                    print("more than one")
+                    CLIENTS[addrs]['w'].write(f"{bcolors.OKBLUE}{addr[0]}:{bcolors.OKGREEN}{addr[1]} {bcolors.HEADER}:> {messList[0]}{bcolors.ENDC}".encode())
+                    await CLIENTS[addrs]["w"].drain()
+                    spaces = " " * len(f'{addr[0]}:{addr[1]}:> ')
+                    for line in messList[1:]:
+                        CLIENTS[addrs]['w'].write(b"\n")
+                        CLIENTS[addrs]['w'].write(f"{spaces} {bcolors.HEADER}{line}{bcolors.ENDC}".encode())
+                        await CLIENTS[addrs]["w"].drain()
+                else:
+                    print("only one")
+                    CLIENTS[addrs]["w"].write(f"{bcolors.OKBLUE}{addr[0]}:{bcolors.OKGREEN}{addr[1]} {bcolors.HEADER}:> {messList[0]}{bcolors.ENDC}".encode())
+                    await CLIENTS[addrs]["w"].drain()
+                CLIENTS[addrs]['w'].write(b"\n")
+                await CLIENTS[addrs]["w"].drain()
+                print(f"message {message} from {addr} to {addrs}")
+            else:
+                print("message not sent to self")
+
+async def main():
+    server = await asyncio.start_server(handle_client_msg, '10.1.1.22', 8888)
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {addrs}')
+
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
