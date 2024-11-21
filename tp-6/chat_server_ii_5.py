8e6525730a73bfe40fc1ac0f909ba98e28dc9fab
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-11-21 16:13:32 +0100

    reorg

--- tp-6/chat_server_ii_5.py
+++ tp-6/chat_server_ii_5.py
@@ -37,6 +37,7 @@ async def handle_client_msg(reader, writer):
         CLIENTS[addr]['pseudo'] = pseudo
 
         for addrs in CLIENTS.keys():
+            print('1')
             if newUsr:
                 CLIENTS[addrs]['w'].write(f"{bcolors.OKBLUE}{CLIENTS[addr]['pseudo']} {bcolors.HEADER} has joined{bcolors.ENDC}".encode())
                 await CLIENTS[addrs]["w"].drain()
